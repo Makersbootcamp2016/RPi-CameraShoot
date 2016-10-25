bd8660669a4b7f5ca78461642083ae5b795896af
maigre <thomas.bohl@gmail.com>
2016-10-25 20:17:52 +0000

    fix installer

--- install.sh
+++ install.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-apt update
-apt install python-requests python-picamera git
+sudo apt update
+sudo apt install python-requests python-picamera git
 
-# Enable Camera
-set_config_var start_x 1 /boot/config.txt
-CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
-if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
-  set_config_var gpu_mem 128 /boot/config.txt
-fi
-sed /boot/config.txt -i -e "s/^startx/#startx/"
-sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
-
-# Clone
-cd /home/pi
-rm -R RPi-CameraShoot
-git clone https://github.com/Makersbootcamp2016/RPi-CameraShoot.git
-chown pi:pi -R RPi-CameraShoot
-
-# Autostart
+echo ""
+echo "Don't forget to enable the camera with raspi-config, and add the script to /etc/rc.local"
 
