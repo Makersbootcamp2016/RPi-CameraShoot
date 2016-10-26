32c0e6bc4c7eece3faeeb4a851e4b85355146b05
maigre <thomas.bohl@gmail.com>
2016-10-26 23:03:49 +0000

    usershot

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python-requests python-picamera git
+sudo apt install python-pip git
+sudo pip install requests picamera
 
 echo ""
 echo "Don't forget to enable the camera with raspi-config, and add the script to /etc/rc.local"
